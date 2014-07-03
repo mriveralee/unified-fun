345c7a86a2e5570e866665dc7aa58c56b1c114bd
Michael Rivera <mrivera.lee@gmail.com>
2014-07-02 23:27:20 -0400

    Added hero who can jump

--- test-2d/2dtest/Assets/MoveLeft.cs
+++ test-2d/2dtest/Assets/MoveLeft.cs
@@ -1,26 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Jump : MonoBehaviour {
+public class MoveLeft : MonoBehaviour {
 
-	public float mJumpForce;
-	private GameObject mHero;
-	private float mDoubleJumpTime;
-	public float mJumpWaitTime;
-	private float mNextJumpTime;
-	private float mNextDoubleJumpTime;
 
+	public Vector3 mMoveSpeed;
+	private bool mIsMoving;
+	private GameObject[] scene;
+	private GameObject mBackground;
+	private
 
 
 	// Use this for initialization
 	void Start () {
-		// Find Hero
-		mHero = GameObject.Find("Hero");
-		mJumpForce = 200f;
-		mJumpWaitTime = 0.78f;
-		mDoubleJumpTime = 0.5f;
-		mNextJumpTime = 0f;
-		mNextDoubleJumpTime = 0f;
+		mIsMoving = false;
+		mMoveSpeed = new Vector3();
+		scene = GameObject.FindGameObjectsWithTag("Moveable");
+		mBackground = GameObject.Find("Background");
 	}
 	
 	// Update is called once per frame
@@ -38,50 +34,36 @@ public class Jump : MonoBehaviour {
 		}
 		else if (Application.platform == RuntimePlatform.OSXEditor) {
 			if (Input.GetMouseButtonDown(0)) {
-			    touchPhase = TouchPhase.Began;
-				hasTouch = true;
+				touchPhase = TouchPhase.Began;
 			} else if (Input.GetMouseButtonUp(0)) {
 				touchPhase = TouchPhase.Ended;
-				hasTouch = true;
 			}
 			touchPos = Input.mousePosition;
+			hasTouch = true;
 		}
 		// Check the touch 
 		if (hasTouch) CheckTouch(touchPos, touchPhase);
-	
+		
 	}
-
 
 	/* Check the current touch coordinate on the screen
 	 * @param pos - the position touched
 	 * @param phase - the phase of 
 	 */ 
 	void CheckTouch (Vector3 pos, TouchPhase phase) {
-		if (phase != TouchPhase.Began && phase != TouchPhase.Ended) {
-			return;
-		} if (phase == TouchPhase.Began && Time.time < mNextJumpTime) { 
+		if (phase != TouchPhase.Began) {
 			return;
 		}
-
+		
 		// Get world position of the hit point
 		Vector3 wp = Camera.main.ScreenToWorldPoint(pos);
 		Vector2 touchPos = new Vector2(wp.x, wp.y);
 		Collider2D hitPt = Physics2D.OverlapPoint(touchPos);
-
-		if (hitPt == null) {
-			return;
-		}
-
+		
 		// Check if we have a hit point
-		if (hitPt.gameObject.name == "JumpButton" && hitPt) {
-			if (phase == TouchPhase.Began) {
-				mHero.rigidbody2D.AddForce(new Vector2(0.0f, mJumpForce));
-				audio.Play();
-				mNextJumpTime = Time.time + mJumpWaitTime;
-			} else if (phase == TouchPhase.Ended) {
-				// Do nothing
-			}
-		}
+		/*if (hitPt && hitPt.gameObject.name == "JumpButton") {
+			mHero.rigidbody2D.AddForce(new Vector2(0f, mJumpForce));
+			audio.Play();
+		}*/
 	}
-
 }
